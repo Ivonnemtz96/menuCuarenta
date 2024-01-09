318907ce21018f3ece5e9917371e9d25d9fbba8c
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2024-01-09 10:10:29 -0700

    se agrega icons

--- includes/footer.php
+++ includes/footer.php
@@ -4,52 +4,10 @@
  </div>
  <!-- End scrollUp  -->
 
- <footer class="spc-large">
+ <footer class="spc-large" style="background-image: url(images/footer/2.jpg); padding-top:39px;">
      <div class="container">
-         <!-- Footer Logo  -->
-         <div class="logo text-center">
-             <a href="index.html"> <img src="images/logo.png" alt="logo"></a>
-         </div>
-         <!-- Footer Social Icon  -->
-         <div class="social-icon text-center">
-             <ul class="text-center">
-                 <li>
-                     <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
-                 </li>
-                 <li>
-                     <a href="#"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
-                 </li>
-                 <li>
-                     <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
-                 </li>
-                 <li>
-                     <a href="#"><i class="fa fa-pinterest-p" aria-hidden="true"></i></a>
-                 </li>
-                 <li>
-                     <a href="#"><i class="fa fa-vimeo" aria-hidden="true"></i></a>
-                 </li>
-             </ul>
-         </div>
-         <div class="row">
-             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 col-md-offset-1">
-                 <div class="address">
-                     503 Old Buffalo Street
-                     Northwest #205, New York.
-                 </div>
-             </div>
-             <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
-                 <div class="phone">
-                     <a href="tel:+3453-909-6565"> +3453-909-6565</a>
-                     <a href="tel:+2390-875-2235">+2390-875-2235</a>
-                 </div>
-             </div>
-             <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                 <div class="email">
-                     <a href="mailto:infoname@gmail.com">infoname@gmail.com</a>
-                     <a href="mailto:www.yourname.com">www.yourname.com</a>
-                 </div>
-             </div>
-             <div class="copyright text-center">© 2017 <span>RS Theme.</span> All Rights Reserved. </div>
-         </div>
+         <div class="copyright text-center" style="margin-top: 0;">
+             © 2024 <span>Cuarenta Seafood.</span> Todos los
+             derechos reservados. <br> Diseñado por <a href="https://bananagroup.mx" target="_blan"><b>Banana Group Marketing</b></a></div>
      </div>
  </footer>
--- includes/head.php
+++ includes/head.php
@@ -1,15 +1,16 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Restaurant | Menu</title>
+    <title>Cuarenta Seafood | Menu</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="apple-touch-icon" href="apple-touch-icon.html">
-    <link rel="shortcut icon" type="image/x-icon" href="images/fav.png">
+    <!-- <link rel="shortcut icon" type="image/x-icon" href="images/fav.png"> -->
     <!-- Place favicon.ico in the root directory -->
     <!-- all css here -->
     <!-- bootstrap v3.3.6 css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <!-- font-awesome css -->
     <link rel="stylesheet" href="css/font-awesome.min.css">
     <!-- flat Icon css -->
